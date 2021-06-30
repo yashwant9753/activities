254ab1aabb7ec19011d6e356deed1dbb0d9cf739
yashwant9753 <ys25701@gmail.com>
2021-06-30 08:21:53 +0530

    initialiize the Firebase the Database

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,36 @@ import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
-  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
     return MaterialApp(
-      title: 'Activity App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        brightness: Brightness.dark,
-      ),
-      home: SignInScreen(),
-    );
+        title: 'Activity App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          brightness: Brightness.dark,
+        ),
+        home: FutureBuilder(
+            future: _fbApp,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                print('You have an error! ${snapshot.error.toString()}');
+                return Text('Something went wrong');
+              } else if (snapshot.hasData) {
+                return SignInScreen();
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            }));
   }
 }
