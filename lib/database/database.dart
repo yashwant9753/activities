7b4b8b684b8d6446809867565fd5897e9e429e0f
yashwant9753 <ys25701@gmail.com>
2021-07-03 09:03:10 +0530

    small changes

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:login/screens/activityScreen.dart';
+import 'package:login/main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-final DatabaseReference = FirebaseDatabase.instance.reference();
+// final DatabaseReference = FirebaseDatabase.instance.reference().child('test');
+final databaseRef = FirebaseDatabase.instance.reference();
